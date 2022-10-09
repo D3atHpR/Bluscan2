804133b881955c07f09b52372efaa930ea3c4587
D3atHpR <kastiel103@gmail.com>
2022-10-09 01:07:31 -0300

    implementação

---
+++ alert.js
@@ -0,0 +1,8 @@
+//const btn = document.querySelector('button');
+//btn.addEventListener('click',function(){
+//    alert("Isso é apesnas um teste");
+//});
+
+function Alert(){
+    alert("Será implementado Logo")
+}
